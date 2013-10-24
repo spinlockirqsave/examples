1481947d3e02451697b290bf86e5c21a28944ad6
cf16 <cf16rocket@gmail.com>
2013-10-24 21:12:05 +0200

    new delete overload

---
+++ cppapp_new_delete_overload/main.cpp
@@ -0,0 +1,61 @@
+/* 
+ * File:   main.cpp
+ * Author: postmaster cf16.eu
+ *
+ * Created on August 27, 2013, 12:15 AM
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+using namespace std;
+
+/*
+ * 
+ */
+class A
+{
+public:
+    A(){printf("A\n");}
+    ~A(){}
+    static void* operator new(size_t t){
+        printf("A new\n");
+        ::operator new(t);
+    }
+    static void operator delete(void* t){
+        printf("A delete\n");
+        ::operator delete(t);
+    }
+    
+    void check_B(){
+        b = new B();
+        ::operator delete(b);
+    }
+
+    class B{
+    public:
+        B(){}
+    };
+    
+    B* b;
+};
+
+class C : public A {
+    
+};
+
+int main(void)
+{
+    A* a = new A;
+    printf("\n------\n");
+    a->check_B();
+    printf("\n------\n");
+    delete a;
+    
+    C* c = new C;
+    A* cc = new C;
+    
+    delete c;
+    delete cc;
+return 0;
+}
+
