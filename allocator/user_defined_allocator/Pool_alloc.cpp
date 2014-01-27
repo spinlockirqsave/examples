1e67ba7f309e57bce78c5a0d9d907d4845c71b1a
cf16 <cf16rocket@gmail.com>
2014-01-27 14:04:15 +0100

    allocate n

--- allocator/user_defined_allocator/Pool_alloc.cpp
+++ allocator/user_defined_allocator/Pool_alloc.cpp
@@ -13,6 +13,9 @@ template< class T> Pool Pool_alloc<T>::mem( sizeof(T));
 template< class T> 
 typename Pool_alloc<T>::pointer Pool_alloc<T>::allocate( size_type n, const void*) {
     if ( n == 1) return static_cast<T*>( mem.alloc());
+    if ( n==2) {
+        int u = 0;
+    }
     return static_cast<T*>( mem.alloc(n));
     //...
 }
@@ -22,6 +25,15 @@ void Pool_alloc<T>::deallocate( pointer p, size_type n) {
     if ( n == 1) {
         mem.free( p);
         return;
+    }
+    //void* last = p + (n-1) * 4;
+    //pointer last = p + (n-1);
+    Pool::Link* last = ( reinterpret_cast< Pool::Link*>( p) + (n-1));
+    while(n) {
+        //pointer* next = (last-4);
+        mem.free(last);
+        last = last - 1;
+        --n;
     }
     //...
 }
