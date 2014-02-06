ddf1ddfcde334ef393bffc90d725d81fb722a206
cf16 <cf16rocket@gmail.com>
2014-02-06 08:25:35 +0100

    cin get

---
+++ io/get/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on February 5, 2014, 9:42 PM
+ */
+
+#include <iostream>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    char buf[10];
+    char c;
+    while ( std::cin)
+    {
+        std::cin.get( buf, 10); // newline is terminator
+        /* get() always leaves terminator in the stream
+         * so it becomes first character in the stream on next read */
+        std::cout << buf;
+        /* oops, forgot to remove '\n' from the cin...
+         * never call get twice without removing the terminator */
+    }
+    return 0;
+}
+
