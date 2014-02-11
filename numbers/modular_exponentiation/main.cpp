287f6258e5ecb76ed56fbebf5b8b24a3d46b4252
cf16 <cf16rocket@gmail.com>
2014-02-11 13:24:36 +0100

    modular exponentiation additional method

--- numbers/modular_exponentiation/main.cpp
+++ numbers/modular_exponentiation/main.cpp
@@ -1,7 +1,6 @@
 /* 
  * File:   main.cpp
  * Author: piter cf16 eu
- * by Vikram Bhat:
  * http://stackoverflow.com/questions/21677292/remainder-when-dividing-large-number/21679387#21679387
  * Created on February 11, 2014, 12:07 PM
  */
@@ -11,14 +10,15 @@
 using namespace std;
 
 /**
+ * by Vikram Bhat
  * Call: modexp(30,74,57) to get (30^74 % 57)
  * Time Complexity: O(log(pow))
  * @param a
  * @param pow
  * @param p
- * @return 
+ * @return remainder
  */
-long modexp(long a, long pow, long p) {
+long modexp( long a, long pow, long p) {
 
     if (pow == 0) {
         return (1);
@@ -35,10 +35,26 @@ long modexp(long a, long pow, long p) {
 
 }
 
+/**
+ * by cf16 
+ * @param base
+ * @param exponent
+ * @param modulus
+ * @return remainder
+ */
+long modular_pow( long base, long exponent, long modulus) {
+    long c = 1;
+    for ( long e_prim = 0; e_prim < exponent; ++e_prim) {
+        c = (c * base) % modulus;
+    }
+    return c;
+}
+
 int main(int argc, char** argv) {
 
     // 30^74 % 57
-    printf( "30^74 % 57:%ld", modexp( 30, 74, 57));
+    printf( "modexp: 30^74 % 57:%ld\n", modexp( 30, 74, 57));
+    printf( "modular_pow: 30^74 % 57:%ld", modular_pow( 30, 74, 57));
     return 0;
 }
 
