8bdb48f32dbb09b19b2f08d31d4dfe062d93451f
cf16 <cf16rocket@gmail.com>
2014-02-24 21:19:26 +0100

    vector init

--- cppapp_vector_init/main.cpp
+++ cppapp_vector_init/main.cpp
@@ -11,16 +11,19 @@ using namespace std;
 
 class A {
 public:
-    A(int i) : i_(i), v_(10) {}
-    A(int i, int j) : i_(i), j_(j) {}
+    //A(int i) : i_(i), v_(10) {}
+    //A(int i, int j) : i_(i), j_(j) {}
     vector< int*> v_;
     int i_;
     int j_;
 };
 
 int main() {
-    A a(9); // v size is 10
-    A b(1, 2); // v size is 0
+    //A a(9); // v size is 10
+    //A b(1, 2); // v size is 0
+    
+    A a1;
+    A a2 = A();
     return 0;
 }
 
