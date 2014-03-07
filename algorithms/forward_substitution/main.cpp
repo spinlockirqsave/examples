78c0b998f52733c4fd510d402a7182dba3fb631a
cf16 <cf16rocket@gmail.com>
2014-03-07 01:17:40 +0100

    solve linear equation system by forward substitution on lower triangular matrix

--- algorithms/forward_substitution/main.cpp
+++ algorithms/forward_substitution/main.cpp
@@ -19,7 +19,7 @@
  * @param x solution
  * http://ktuce.ktu.edu.tr/~pehlivan/numerical_analysis/chap02/BackSubstitution.pdf
  */
-void solve(int n, float a[4][4], float b[], float x[]) {
+void solve1( int n, float a[][4], float b[], float x[]) {
   float s;
 
   for( int i = 0; i < n; i++) {
@@ -28,6 +28,18 @@ void solve(int n, float a[4][4], float b[], float x[]) {
             s = s + a[ i][ j] * x[ j];
         }
         x[ i] = ( b[ i] - s) / a[ i][ i];
+   }
+}
+
+void solve2( int n, float* a, float b[], float x[]) {
+  float s;
+
+  for( int i = 0; i < n; i++) {
+        s = 0;
+        for( int j = 0; j < i; j++) {
+            s = s + a[ i * n + j] * x[ j];
+        }
+        x[ i] = ( b[ i] - s) / a[ i * n + i];
    }
 }
 
@@ -40,7 +52,9 @@ int main(int argc, char** argv) {
     float a[4][4] = { 3, 0, 0, 0, -1, 1, 0, 0, 3, -2, -1, 0, 1, -2, 6, 2};
     float b[4] = { 5, 6, 4, 2};
     float res[4];
-    solve( n, a, b, res);
+    solve1( n, a, b, res);
+    std::copy( res, res + 4, std::ostream_iterator<float>( std::cout, ","));
+    solve2( n, &a[0][0], b, res);
     std::copy( res, res + 4, std::ostream_iterator<float>( std::cout, ","));
     return 0;
 }
