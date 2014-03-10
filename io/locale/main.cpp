112d864a1fd34807c2d3a88928d552559b58e1cb
cf16 <cf16rocket@gmail.com>
2014-03-10 22:54:56 +0100

    locale

---
+++ io/locale/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on March 10, 2014, 10:46 PM
+ */
+
+#include <locale>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    std::locale loc0;                 // copy of the current global locale
+    std::locale loc1 = std::locale(); // copy of the current global locale
+    std::locale loc2(" ");             // copy of ‘‘the user’s preferred locale’’
+    std::locale loc3( "C");       // copy of the "C" locale
+    std::locale loc4 = std::locale::classic();  // copy of the "C" locale
+    std::locale loc5( "POSIX"); // copy of the implementation-defined "POSIX" locale
+
+    return 0;
+}
+
