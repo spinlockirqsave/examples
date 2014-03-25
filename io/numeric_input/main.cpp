3c9446dad370aa87b10b567df6d2db4f55939cf6
cf16 <cf16rocket@gmail.com>
2014-03-25 12:17:11 +0100

    numeric input using custom facet

--- io/numeric_input/main.cpp
+++ io/numeric_input/main.cpp
@@ -8,16 +8,15 @@
 #include <iostream>
 #include <locale>
 
-class my_numpunct : public std::numpunct<char> {
-    public:
+struct my_numpunct : public std::numpunct<char> {
         typedef char char_type;
         typedef std::string string_type;
         
         explicit my_numpunct( size_t r = 0) : std::numpunct<char>( r) {}
-    protected:
-        char do_decimal_point() const { return '+';}  // comma
+
+//        char do_decimal_point() const { return ',';}  // comma
         char do_thousands_sep() const { return ' ';}  // space
-        string_type do_groupig() const { return "\003";}   // 3-digit groups
+        string_type do_grouping() const { return "\1\2\3";}   // 3-digit groups
 };
 
 
@@ -30,15 +29,19 @@ int main(int argc, char** argv) {
     int i1;
     double d1;
     std::cin >> i1 >> d1; // read using standard "12345678" format
+    std::cout << i1 << d1;
+    std::cout << 12345678;
     
-    std::locale loc( std::locale::classic(), new my_numpunct);
-    std::cin.imbue( loc);
+    std::cin.imbue( std::locale( std::cin.getloc(), new my_numpunct));
+    std::cout.imbue( std::locale( std::cout.getloc(), new my_numpunct));
+    std::cout << 12345678;
     
     std::cout << "Style B:";
     int i2;
     double d2;
     std::cin >> i2 >> d2; // read using standard "12 345 678" format
+    std::cout << i2 << d2;
+    std::cout << 12345678;
     
     return 0;
 }
-
