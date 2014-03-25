d3994e88efea75de6f3408a50a36f47ce2eeaee7
cf16 <cf16rocket@gmail.com>
2014-03-25 14:44:13 +0100

    convert date format using facet & boost

--- io/convert_date_format/main.cpp
+++ io/convert_date_format/main.cpp
@@ -1,6 +1,7 @@
 /* 
  * File:   main.cpp
  * Author: piter cf16 eu
+ * http://stackoverflow.com/questions/14099542/c-convert-date-formats
  *
  * Created on March 25, 2014, 2:35 PM
  */
@@ -18,10 +19,10 @@ int main(int argc, char **argv)
 {
     int dateIn = 20111201;
 
-    // Read the date in from ISO format as an int.
+    // Read the date in from ISO format as an int
     ostringstream ss;
     ss << dateIn;
-    date d(from_undelimited_string( ss.str() ));
+    date d( from_undelimited_string( ss.str() ));
 
     // Set the output format
     date_facet *fct = new date_facet( "%d%b%Y");    // [1]
