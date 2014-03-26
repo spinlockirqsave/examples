48dd44f7e24e46abc06e89470c08532db0c2a15d
cf16 <cf16rocket@gmail.com>
2014-03-26 17:59:17 +0100

    date & time input/output using C++ streams

--- io/date_and_time_io/main.cpp
+++ io/date_and_time_io/main.cpp
@@ -14,6 +14,15 @@ public:
     class Bad_date {};
     Date( int dd, Month mm, int yy);
     Date();
+    Date( Date& other) : fmt_in( other.fmt_in), fmt_out( other.fmt_out) {
+        d = other.d;
+    }
+    Date& operator=( Date const& other) {
+        fmt_in = other.fmt_in;
+        fmt_out = other.fmt_out;
+        d = other.d;
+        return *this;
+    }
     
     friend std::ostream& operator<<( std::ostream&, const Date& d);
     char fmt_in, fmt_out;
@@ -29,11 +38,14 @@ Date::Date( int dd, Month mm, int yy) {
     x.tm_mon = mm -1;      // tm_mon is zero based
     x.tm_year = yy - 1900; // tm_year is 1900 based
     d = mktime( &x);
+    fmt_in = 'x';
+    fmt_out = 'x';
 }
 
 Date::Date() {
     d = time( 0); // default Date: today
     if( d == time_t( -1)) throw Bad_date();
+    fmt_in = 'x';
     fmt_out = 'x';
 }
 /*
@@ -53,6 +65,25 @@ std::ostream& operator<<( std::ostream& s, const Date& d) {
     return s;
 }
 
+std::istream& operator>>( std::istream& s, Date& d) {
+    std::istream::sentry guard( s);
+    if( !guard) return s;
+    
+    std::ios_base::iostate res = std::ios_base::goodbit;
+    tm x = { 0};
+    std::istreambuf_iterator<char, std::char_traits<char> > end;
+    try {
+        std::use_facet< std::time_get<char> >( s.getloc()).get_date( s, end, s, res, &x);
+        if( res == 0 || res == std::ios_base::eofbit)
+            //d = Date( x.tm_mday, Date::Month( x.tm_mon + 1), x.tm_year + 1900);
+            d.operator=( Date( x.tm_mday, Date::Month( x.tm_mon + 1), x.tm_year + 1900));
+        //else
+            //s.setstate( res);    
+    } catch( std::exception& e) {
+        std::cout << e.what();
+    }
+}
+
 /*
  *
  */
@@ -60,6 +91,11 @@ int main(int argc, char** argv) {
 
     Date today;
     std::cout << today << std::endl;  // write using %x format: 03/26/14
+    
+    Date dd;
+    std::cin >> dd;               // read dates produced by %x format
+    std::cout << dd << std::endl; // write using %x format
+    
     today.fmt_out = 'a';
     std::cout << today << std::endl;  // write using %a format: abbreviated weekday name: Wed
     today.fmt_out = 'b';
@@ -76,6 +112,7 @@ int main(int argc, char** argv) {
     std::cout << today << std::endl;  // write using %j format: day of year: 085
     today.fmt_out = 'p';
     std::cout << today << std::endl;  // write using %p format: a.m./p.m.: PM
+
     return 0;
 }
 
