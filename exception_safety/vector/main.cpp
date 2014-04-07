39ee0ff757f9f76e3335c6b739f1b887f9b5c0e9
cf16 <cf16rocket@gmail.com>
2014-04-07 14:10:45 +0200

    handling exceptions in constructor

--- exception_safety/vector/main.cpp
+++ exception_safety/vector/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <algorithm>
 
 template<typename T, typename A = std::allocator<T> >
 class vector {
@@ -29,6 +30,7 @@ class vector {
         //...
 };
 
+/* approach similar to std::uninitialized fill taken */
 template<typename T, typename A >
 vector<T,A>::vector( size_type n, const T& val, const A& a) : alloc( a)  // copy the allocator
 {
@@ -40,7 +42,7 @@ vector<T,A>::vector( size_type n, const T& val, const A& a) : alloc( a)  // copy
     try {
         iterator end = v + n;
         for( p = v; p != end; ++p)
-            alloc.construct( p, val); /* construct element (placement new):
+            alloc.construct( p, val); /* construct elements (placement new):
                                       e g. void construct( pointer p, const T& val) 
                                       { ::new((void *)p) T( val); } */
         last = space = p;
@@ -50,13 +52,60 @@ vector<T,A>::vector( size_type n, const T& val, const A& a) : alloc( a)  // copy
         alloc.deallocate( v, n);     /* free memory */
         throw;                       /* re-throw to signal constructor that failed */
     }
-   
 }
 
 template<typename T, typename A >
 void vector<T,A>::push_back( const T& val) {}
 
+/* ------------------*/
 
+/* improved implementation of above vector */
+template<typename T, typename A = std::allocator<T> >
+class vector2 {
+    private:
+        T* v;        // start of allocation
+        T* space;    // end of element sequence, start of space allocated for possible expansion
+        T* last;     // end of allocated space
+        A alloc;     // allocator
+        typedef T       value_type;
+        typedef size_t	size_type;
+        typedef T* iterator;
+    public:
+        explicit vector2( size_type n, const T& val = T(), const A& = A());
+        vector2( const vector2& other);
+        vector2& operator=( const vector2& other);
+        ~vector2();
+        
+        size_type size() const { return space - v; }  // pointer arithmetic
+        size_type capacity() const { return last - v; }
+        void push_back( const T&);
+        //...
+};
+
+/* approach similar to std::uninitialized fill taken */
+template<typename T, typename A >
+vector2<T,A>::vector2( size_type n, const T& val, const A& a) : alloc( a)  // copy the allocator
+{
+    /* keep track of which elements have been constructed
+     * and destroy those and only those in case of exception */
+    v = alloc.allocate( n); // get memory for elements
+    iterator p;             // declared before try{} so it is still valid in catch{} block
+    
+    try {
+        iterator end = v + n;
+        std::uninitialized_fill( v, v + n, val); /* copy elements with (placement new):
+                                                  e g. void construct( pointer p, const T& val) 
+                                                  { ::new((void *)p) T( val); } */
+        last = space = v + n;
+    } catch( ...) {
+        /* destructors have been already called by uninitialized_fill */
+        alloc.deallocate( v, n);     /* free memory */
+        throw;                       /* re-throw to signal constructor that failed */
+    }
+}
+
+template<typename T, typename A >
+void vector2<T,A>::push_back( const T& val) {}
 /*
  * 
  */
