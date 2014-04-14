8aae2a68d0efd9b52091afa3d4c9f926f422af59
cf16 <cf16rocket@gmail.com>
2014-04-14 22:31:24 +0200

    Stirling estimation for binomial coefficient

--- algorithms/stirling/main.cpp
+++ algorithms/stirling/main.cpp
@@ -10,6 +10,49 @@
 
 #define _USE_MATH_DEFINES
 
+/**
+ * Calculates the binomial coefficient, $\choose{n, k}$, i.e., the number of
+ * distinct sets of $k$ elements that can be sampled with replacement from a
+ * population of $n$ elements.
+ *
+ * @tparam T
+ *   Numeric type. Defaults to unsigned long.
+ * @param n
+ *   Population size.
+ * @param k
+ *   Number of elements to sample without replacement.
+ *
+ * @return
+ *   The binomial coefficient, $\choose{n, k}$.
+ *
+ * @note
+ *    Modified from: http://etceterology.com/fast-binomial-coefficients
+ */
+template <class T>
+T binomial_coefficient(unsigned long n, unsigned long k) {
+    unsigned long i;
+    T b;
+    if (0 == k || n == k) {
+        return 1;
+    }
+    if (k > n) {
+        return 0;
+    }
+    if (k > (n - k)) {
+        k = n - k;
+    }
+    if (1 == k) {
+        return n;
+    }
+    b = 1;
+    for (i = 1; i <= k; ++i) {
+        b *= (n - (k - i));
+        if (b < 0) return -1; /* Overflow */
+        b /= i;
+    }
+    return b;
+}
+
 double stirling_lower_bound( int n) {
     double n_ = n / 2.0;
     double res = pow( 2.0, 2 * n_);
@@ -32,17 +75,34 @@ int stirling_estimate( double x) {
     }
     return n;
 }
+
+long int search_coefficient( unsigned long int &n, unsigned long int x) {
+    unsigned long int k = n / 2;
+    long long middle_coefficient = binomial_coefficient<long long>( n, k);
+    if( middle_coefficient == x) return k;
+
+    unsigned long int right = binomial_coefficient<unsigned long>( n, ++k);
+    while ( x != right) {
+        
+        while( x < right ||  x < ( right * ( n + 1) / ( k + 1))) {
+            //right = binomial_coefficient<unsigned long>( n, ++k);
+            right = right * ( n + 1) / ( ++k) - right;
+        }
+        if ( right == x) return k;
+        right = right * ( ++n) / ( ++k);
+        if( right > x) return -1;
+    }
+    return k;
+}
 /*
  * 
  */
 int main(int argc, char** argv) {
-
-    int n = 1;
-    double x1 = 20;
-    std::cout << "Stirling estimate: " << stirling_estimate( x1);
     
-    double x2 = pow( 10, 100);
-    std::cout << "Stirling estimate: " << stirling_estimate( x2);
+    long long x2 = 1365;
+    unsigned long int n = stirling_estimate( x2);
+    long int k = search_coefficient( n, x2);
+    std::cout << "row:" << n <<", column: " << k; 
     return 0;
 }
 
