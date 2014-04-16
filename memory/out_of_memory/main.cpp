8847453ecad2534a9ad17baa9f8e78d44ea6cf02
cf16 <cf16rocket@gmail.com>
2014-04-16 17:55:12 +0200

    new handler support class specific

--- memory/out_of_memory/main.cpp
+++ memory/out_of_memory/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstdio>
 
 typedef void ( *new_handler)();
 
@@ -14,7 +15,8 @@ template<class T>                    // "mixin-style" base class
 class NewHandlerSupport {            // for class specific 
 public:                              // set_new_handler support
     static new_handler set_new_handler(new_handler p);
-    static void * operator new(size_t size);
+    static void* operator new(size_t size);
+    static void* operator new[](size_t size);
 
 private:
     static new_handler currentHandler;
@@ -50,59 +52,92 @@ void* NewHandlerSupport<T>::operator new(size_t size) {
     return memory;
 }
 
-// note inheritance from mixin base class template. (See
-// my article on counting objects for information on why
+template<class T> 
+void* NewHandlerSupport<T>::operator new[](size_t size) {
+    new_handler globalHandler = // install X's
+            std::set_new_handler(currentHandler);       // handler
+
+    void *memory;
+
+    try {                                               // attempt
+        memory = ::operator new[](size);                  // allocation
+    } catch (std::bad_alloc&) {                         // restore
+        std::set_new_handler(globalHandler);            // handler;
+        throw;                                          // propagate exception
+    }
+
+    std::set_new_handler(globalHandler);                // restore handler
+    return memory;
+}
+
+// note inheritance from mixin base class template.
 // private inheritance might be preferable here.)
 
 class X : public NewHandlerSupport<X> {
    // as before, but no declarations for
 }; // set_new_handler or operator new
 
-class Y : public NewHandlerSupport<X> {
+class Y : public NewHandlerSupport<Y> {
    // as before, but no declarations for
 }; // set_new_handler or operator new
 
 
     
-void noMoreMemoryForX(){}                          // decl. of function to
-                                                   // call if memory allocation
-                                                   // for X objects fails
-void noMoreMemoryForY(){}
+void noMoreMemoryForX() {
+    std::printf( "noMoreMemoryForX");
+    throw std::bad_alloc();
+}
+
+void noMoreMemoryForY() {
+    std::printf( "noMoreMemoryForX");
+    throw std::bad_alloc();
+}
     
 /*
  * 
  */
 int main(int argc, char** argv) {
 
-    X::set_new_handler(noMoreMemoryForX);
+    X::set_new_handler( noMoreMemoryForX);
                                                    // set noMoreMemoryForX as X's
                                                    // new-handling function
-
-    X *px1 = new X;                                // if memory allocation
+    size_t s = 0x400000000; // 16GB
+    try {
+    X *px1 = (X*) X::operator new(s);//new X[s];                                // if memory allocation
                                                    // fails, call noMoreMemory
-
-    std::string *ps = new std::string;             // if memory allocation
-                                                   // fails, call the global
-                                                   // new-handling function
-                                                   // (if there is one)
-
-    X::set_new_handler(0);                         // set the X-specific
-                                                   // new-handling function
-                                                   // to nothing (i.e., null)
-
-    X *px2 = new X;                                // if memory allocation
-                                                   // fails, throw an exception
-                                                   // immediately. (There is
-                                                   // no new-handling function
-                                                   // for class X.)
+    } catch (std::bad_alloc&) { 
+        printf( "bad_alloc catched");
+    }
     
-    Y::set_new_handler(noMoreMemoryForX);
-    Y::set_new_handler(noMoreMemoryForY);
-                                                   // set noMoreMemoryForY as Y's
-                                                   // new-handling function
-
-    Y *py1 = new Y;                                // if memory allocation
+    try {
+    X *px1 = new X[s];//new X[s];                                // if memory allocation
                                                    // fails, call noMoreMemory
+    } catch (std::bad_alloc&) { 
+        printf( "bad_alloc catched");
+    }
+
+//    std::string *ps = new std::string;             // if memory allocation
+//                                                   // fails, call the global
+//                                                   // new-handling function
+//                                                   // (if there is one)
+//
+//    X::set_new_handler(0);                         // set the X-specific
+//                                                   // new-handling function
+//                                                   // to nothing (i.e., null)
+//
+//    X *px2 = new X[s];                                // if memory allocation
+//                                                   // fails, throw an exception
+//                                                   // immediately. (There is
+//                                                   // no new-handling function
+//                                                   // for class X.)
+//    
+//    Y::set_new_handler( noMoreMemoryForX);
+//    Y::set_new_handler( noMoreMemoryForY);
+//                                                   // set noMoreMemoryForY as Y's
+//                                                   // new-handling function
+//
+//    Y *py1 = new Y[s];                                // if memory allocation
+//                                                   // fails, call noMoreMemory
 
 
     return 0;
