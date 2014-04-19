5c4ac2b70caaec45d0a619aceba0909910548b29
cf16 <cf16rocket@gmail.com>
2014-04-19 03:55:32 +0200

    pop from stack of tasks

--- threads/threads_job_to_do/main.cpp
+++ threads/threads_job_to_do/main.cpp
@@ -23,7 +23,7 @@
 typedef boost::mt19937 RNGType;
 
 #define NUM_THREADS  3
-#define TCOUNT 100
+#define TCOUNT 200
 #define COUNT_LIMIT 0
 
 int     count = 0;
@@ -48,6 +48,7 @@ struct thread_data
 };
 
 void* inc_count(void* threadarg) {
+    fprintf( stderr,"Starting inc_count(): thread %ld\n", syscall( SYS_gettid));
     int i;
     struct thread_data* data;
     data = (thread_data*) threadarg;
@@ -62,7 +63,7 @@ sleep(1);
             // do the Job
             double task = data->jobToBeDone.top();
             data->jobToBeDone.pop();
-            printf("inc_count(): thread %ld, popped element = %f  Job done. ",
+            fprintf( stderr, "inc_count(): thread %ld, popped element = %f  Job done. ",
                     syscall(SYS_gettid), task);
         }
         /* 
@@ -72,14 +73,14 @@ sleep(1);
         if (data->jobToBeDone.empty()) {
             data->done_ = true;
             pthread_cond_signal(&count_threshod_cv);
-            printf("Just sent signal.\n");
-            printf("inc_count(): thread %ld, stack size = %lu, unlocking mutex\n",
+            fprintf( stderr, "Just sent signal.\n");
+            fprintf( stderr, "inc_count(): thread %ld, stack size = %lu, unlocking mutex\n",
                 syscall(SYS_gettid), data->jobToBeDone.size());
         pthread_mutex_unlock(&count_mutex);
             break;
         }
         
-        printf("inc_count(): thread %ld, stack size = %lu, unlocking mutex\n",
+        fprintf( stderr, "inc_count(): thread %ld, stack size = %lu, unlocking mutex\n",
                 syscall(SYS_gettid), data->jobToBeDone.size());
         pthread_mutex_unlock(&count_mutex);
 
@@ -96,7 +97,7 @@ void* watch_count(void* threadarg) {
   double task = data->jobToBeDone.top();
   long unsigned int stackSize = data->jobToBeDone.size();
 
-  printf("Starting watch_count(): thread %ld\n", syscall(SYS_gettid));
+  fprintf( stderr,"Starting watch_count(): thread %ld\n", syscall(SYS_gettid));
 
   /*
   Lock mutex and wait for signal.  Note that the pthread_cond_wait routine
@@ -107,17 +108,17 @@ void* watch_count(void* threadarg) {
   */
   pthread_mutex_lock(&count_mutex);
   //while (count < COUNT_LIMIT) {
-    printf("watch_count(): thread %ld stack size= %lu. Going into wait...\n", syscall(SYS_gettid), stackSize);
+    fprintf( stderr, "watch_count(): thread %ld stack size= %lu. Going into wait...\n", syscall(SYS_gettid), stackSize);
     pthread_cond_wait(&count_threshod_cv, &count_mutex);
-    printf("watch_count(): thread %ld Condition signal received. stack size= %lu\n", syscall(SYS_gettid), data->jobToBeDone.size());
-    printf("watch_count(): thread %ld Updating the value of stack...\n", syscall(SYS_gettid));
+    fprintf( stderr, "watch_count(): thread %ld Condition signal received. stack size= %lu\n", syscall(SYS_gettid), data->jobToBeDone.size());
+    fprintf( stderr, "watch_count(): thread %ld Updating the value of stack...\n", syscall(SYS_gettid));
 //    for(int i=0; i<10; ++i) {
 //           data->jobToBeDone.push(47);
 //       }
-    printf("watch_count(): maybe later. Thank you very much.\n", syscall(SYS_gettid));
-    printf("watch_count(): thread %ld stack size now = %lu.\n", syscall(SYS_gettid), data->jobToBeDone.size());
+    fprintf( stderr, "watch_count(): maybe later. Thank you very much.\n", syscall(SYS_gettid));
+    fprintf( stderr, "watch_count(): thread %ld stack size now = %lu.\n", syscall(SYS_gettid), data->jobToBeDone.size());
     //}
-  printf("watch_count(): thread %ld Unlocking mutex.\n", syscall(SYS_gettid));
+  fprintf( stderr, "watch_count(): thread %ld Unlocking mutex.\n", syscall(SYS_gettid));
   pthread_mutex_unlock(&count_mutex);
   pthread_exit(NULL);
 }
@@ -128,7 +129,7 @@ int main(int argc, char *argv[]) {
   pthread_t threads[3];
   pthread_attr_t attr;
   
-  thread_data data(15);
+  thread_data data(150);
   //data.numberOfTasks_ = 30;
 
   /* Initialize mutex and condition variable objects */
@@ -138,8 +139,9 @@ int main(int argc, char *argv[]) {
   /* For portability, explicitly create threads in a joinable state */
   pthread_attr_init(&attr);
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    pthread_create(&threads[1], &attr, inc_count, (void *) &data);
   pthread_create(&threads[0], &attr, watch_count, (void *) &data);
-  pthread_create(&threads[1], &attr, inc_count, (void *) &data);
+//  pthread_create(&threads[1], &attr, inc_count, (void *) &data);
   pthread_create(&threads[2], &attr, inc_count, (void *) &data);
 
   /* Wait for all threads to complete */
@@ -147,7 +149,7 @@ int main(int argc, char *argv[]) {
     pthread_join(threads[i], NULL);
   }
   
-  printf ("Main(): Waited and joined with %d threads. Final size of stack with tasks = %lu. Done.\n", 
+  fprintf( stderr, "Main(): Waited and joined with %d threads. Final size of stack with tasks = %lu. Done.\n", 
           NUM_THREADS, data.jobToBeDone.size());
 
   /* Clean up and exit */
