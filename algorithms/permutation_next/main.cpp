3a4c23ecf8ce76ea1e0d7a00ee46fc05e3971115
cf16 <cf16rocket@gmail.com>
2014-04-21 19:47:49 +0200

    std::next_permutation

---
+++ algorithms/permutation_next/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on April 21, 2014, 7:38 PM
+ */
+
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <iterator>
+ 
+int main()
+{
+    std::string s = "aba";
+    std::sort(s.begin(), s.end());
+    do {
+        std::cout << s << '\n';
+    } while(std::next_permutation(s.begin(), s.end()));
+    
+    int a[ 4] = { 1, 2, 3, 4};
+    std::vector<int> v( a, a + 4);
+    std::sort( v.begin(), v.end());
+    do {
+        std::copy( v.begin(), v.end(), std::ostream_iterator<int>( std::cout, ","));
+        std::cout << std::endl << std::flush;
+    } while( std::next_permutation(v.begin(), v.end()));
+    
+}
+
