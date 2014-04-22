4e66d961c1b379f804a8c1174b9bac7bc10f5527
cf16 <cf16rocket@gmail.com>
2014-04-22 17:11:42 +0200

    graph construction with Boost GBL

---
+++ algorithms/graph/graph_construction/main.cpp
@@ -0,0 +1,68 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on April 22, 2014, 4:30 PM
+ */
+
+#include <iostream>                  // for std::cout
+#include <utility>                   // for std::pair
+#include <algorithm>                 // for std::for_each
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
+using namespace boost;
+
+/*
+ * 
+ */
+int main(int, char*[]) {
+    // create a typedef for the Graph type
+    typedef adjacency_list<vecS, vecS, bidirectionalS> Graph;
+
+    // Make convenient labels for the vertices
+
+    enum {
+        A, B, C, D, E, N
+    };
+    const int num_vertices = N;
+    const char* name = "ABCDE";
+
+    // writing out the edges in the graph
+    typedef std::pair<int, int> Edge;
+    Edge edge_array[] ={ Edge( A, B), Edge( A, D), Edge( C, A), Edge( D, C),
+        Edge( C, E), Edge( B, D), Edge( D, E), Edge( B, A)};
+    const int num_edges = sizeof (edge_array) / sizeof ( edge_array [0]);
+
+    // declare a graph object
+    Graph g(num_vertices);
+    // Graph g(edge_array, edge_array + sizeof(edge_array) / sizeof(Edge), num_vertices);
+
+    // add the edges to the graph object
+    for (int i = 0; i < num_edges; ++i)
+        add_edge( edge_array[ i].first, edge_array[ i].second, g);
+    //...
+    
+    // get the property map for vertex indices
+    typedef property_map< Graph, vertex_index_t>::type IndexMap;
+    IndexMap index = get( vertex_index, g);
+
+    std::cout << "vertices( g) = ";
+    typedef graph_traits< Graph>::vertex_iterator vertex_iter;
+    std::pair< vertex_iter, vertex_iter> vp;
+    for ( vp = vertices(g); vp.first != vp.second; ++vp.first)
+      std::cout << index[ *vp.first] <<  " ";
+    std::cout << std::endl;
+    
+    // ...
+    std::cout << "edges( g) = ";
+    graph_traits<Graph>::edge_iterator ei, ei_end;
+    for ( tie(ei, ei_end) = edges( g); ei != ei_end; ++ei)
+        std::cout << "(" << index[ source( *ei, g)] 
+                  << "," << index[ target( *ei, g)] << ") ";
+    std::cout << std::endl;
+    
+    return 0;
+}
+
---
+++ algorithms/graph/undirected_graph/main.cpp
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on April 22, 2014, 4:30 PM
+ */
+
+#include <iostream>                  // for std::cout
+
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/undirected_graph.hpp>
+
+using namespace boost;
+
+/*
+ * 
+ */
+int main(int, char*[]) {
+
+    typedef property < edge_weight_t, double >Weight;
+    typedef adjacency_list < vecS, vecS, undirectedS,
+            no_property, Weight > UndirectedGraph;
+
+    const int V = 3;
+    UndirectedGraph undigraph( V);
+    graph_traits < UndirectedGraph >::vertex_descriptor zero, one, two;
+    graph_traits < UndirectedGraph >::out_edge_iterator out, out_end;
+    graph_traits < UndirectedGraph >::in_edge_iterator in, in_end;
+
+    zero = vertex(0, undigraph);
+    one = vertex(1, undigraph);
+    two = vertex(2, undigraph);
+    add_edge(zero, one, undigraph);
+    add_edge(zero, two, undigraph);
+    add_edge(one, two, undigraph);
+
+    std::cout << "out_edges(0): ";
+    for (boost::tie(out, out_end) = out_edges(zero, undigraph); out != out_end; ++out)
+        std::cout << *out;
+    std::cout << std::endl << "in_edges(0): ";
+    for (boost::tie(in, in_end) = in_edges(zero, undigraph); in != in_end; ++in)
+        std::cout << *in;
+    std::cout << std::endl;
+    return 0;
+}
+
+
+
