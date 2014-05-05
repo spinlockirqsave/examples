e4e819046c93adbe0bd5da76e0db9c74870a5a8f
cf16 <cf16rocket@gmail.com>
2014-05-05 23:20:31 +0200

    implementation of memcpy

---
+++ memory/memcpy_fast/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on May 5, 2014, 11:01 PM
+ */
+
+#include <iostream>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
