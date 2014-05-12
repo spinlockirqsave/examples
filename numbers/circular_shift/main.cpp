687c1eb451b3fbe67ab36d9949250c2b832551d6
cf16 <cf16rocket@gmail.com>
2014-05-12 15:56:16 +0200

    circular shift

--- numbers/circular_shift/main.cpp
+++ numbers/circular_shift/main.cpp
@@ -16,20 +16,25 @@
  */
 int main(int argc, char** argv) {
 
-    uint32_t _arg1 = 0xc2034000;
-    uint32_t _arg2 = 2;
+    uint32_t _arg1 = 0xc2034000;        // 11000010000000110100000000000000
+    uint32_t _arg2 = 2;                 // 00000000000000000000000000000010
+
     std::cout << std::bitset<32>( _arg1) << '\n';
     std::cout << std::bitset<32>( _arg2) << '\n';
 
     /* c = ((_arg1 << _arg2) | (_arg1 >>> (32 - _arg2))) */
     uint32_t a = _arg1 << _arg2;
     std::cout << std::bitset<32>( a) << '\n';
+    // 00001000000011010000000000000000
+
     
     uint32_t b = _arg1 >> ( 32 - _arg2);
     std::cout << std::bitset<32>( b) << '\n';
+    // 00000000000000000000000000000011
     
     uint32_t c = a | b;
     std::cout << std::bitset<32>( c) << '\n';
+    // 00001000000011010000000000000011
 
     std::cout << sizeof(int);
     std::cout << sizeof( uint32_t);
