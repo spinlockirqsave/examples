ddf65afeb16e25fd99e0f2dbb891138915fc6ade
cf16 <cf16rocket@gmail.com>
2014-05-12 16:26:17 +0200

    check the number of sums less than number

--- numbers/possible_sum/main.cpp
+++ numbers/possible_sum/main.cpp
@@ -1,6 +1,6 @@
 /* 
  * File:   main.cpp
- * Author: root
+ * Author: piter cf16 eu
  *
  * Created on January 19, 2014, 8:47 PM
  */
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <iterator>
 
+/* check the number of sums less than number */
 void f( std::vector<int> v, int number) {
     //std::copy( v.begin(), v.end(), std::ostream_iterator<int>(std::cout,","));
     //std::cout << std::endl;
@@ -39,9 +40,7 @@ int g( int number) {
     f( ones, number);
     return map.size();
 }
-/*
- * 
- */
+
 
 std::vector<int> f1() {
     std::vector<int> v(1, 1);
@@ -53,65 +52,9 @@ std::vector<int> f2() {
     return v;
 }
 
-//std::vector<int> h( int number, int current, std::vector<int> vin, int sum) { //10,10,10,0
-//    if ( sum == 1) {
-//        std::vector<int> res(vin);
-//        res.push_back(1); // 9,1
-//        std::copy( res.begin(), res.end(), std::ostream_iterator<int>(std::cout,","));
-//        std::cout << std::endl;
-//    }
-//    
-//    if ( sum == 2) {
-//        std::vector<int> res(vin); // 8
-//        res.resize( res.size()+2, 1); // 8,,1,1
-//        std::copy( res.begin(), res.end(), std::ostream_iterator<int>(std::cout,","));
-//        std::cout << std::endl;
-//        
-//        res.clear();
-//        res(vin);  // 8
-//        res.resize( res.size()+1, 2); // 8,2
-//        std::copy( res.begin(), res.end(), std::ostream_iterator<int>(std::cout,","));
-//        std::cout << std::endl;
-//    }
-//    
-//    std::copy(vin.begin(), vin.end(), std::ostream_iterator<int>(std::cout, ",")); // 10
-//    std::cout << std::endl;
-//    
-//    if (current > 1) { // 10
-//        --current;
-//        vin.clear();
-//        vin.push_back( current); // 9
-//        vin.push_back( sum+1);
-//        h(10,current,vin,sum+1);
-//    }
-//    return v;
-//}
-
-//std::vector<int> fn(int number, int sumres, std::vector<int> vfn) { //5,0
-//    if (sumres == 2) return std::vector<int>(2, 1);
-//    if (sumres == 1) return std::vector<int>(1, 1);
-//
-//    for (int i = number; i; --i) {
-//        std::vector<int> vres;
-//        vres.push_back(i);
-//        if (sumres)
-//            fn( i-1, sumres + 1, vres);
-//    }
-//    return v;
-//}
-
-int ff( int x) {
-    int& x_ref = x;
-    int b = 3;
-    x_ref = b;
-    x++;
-    int* c = &x;
-    return *c - x_ref;
-}
 int main(int argc, char** argv) {
 
-    //g(5);
-    int i = ff(1);
+    g(5);
     return 0;
 }
 
