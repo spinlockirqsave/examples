a347211479d26b1b9fa6ec180d7e31f37193b315
cf16 <cf16rocket@gmail.com>
2014-05-29 15:29:47 +0200

    cantor pairing function as a perfect hash

---
+++ numbers/hash_pairing_function/main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: piter cf16 eu
+ *
+ * Created on May 29, 2014, 3:24 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int cantor_pairing( int k1, int k2) {
+    return 0.5 * ( k1 + k2) * ( k1 + k2 + 1) + k2;
+}
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
