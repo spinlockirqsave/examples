5b50014c4cd2112c1392685c9aca2e86245e8634
cf16 <cf16rocket@gmail.com>
2014-06-14 14:29:20 +0200

    append processed integer from each line to the end of each line in file

---
+++ io/file_append_to_end_of_line/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: piter
+ *
+ * Created on June 14, 2014, 2:06 PM
+ */
+
+
+/*
+ * 
+ */
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+int main() {
+    
+    std::string filename = "file.txt", line;
+    std::ifstream ifs( filename.c_str(), std::ios::in);
+    
+    if (!ifs) // check if the file exists and can be read
+        throw std::runtime_error( "cannot open the file");
+
+    std::stringstream buffer; // buffer to store the lines
+    
+    while ( getline( ifs, line)) {
+        
+        int previous_value;
+        std::istringstream iss(line.substr(line.find_last_not_of(" ")));
+        iss >> previous_value;
+        buffer << line << "\t" << (previous_value + 2) << "\n"; // add integer
+    }
+
+    std::ofstream ofs( filename.c_str(), std::ios::out); // ofstream buffer
+    ofs << buffer.rdbuf(); // write to the file
+
+    return 0;
+}
+
