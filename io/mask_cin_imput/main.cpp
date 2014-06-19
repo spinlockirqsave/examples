e1450f5da8399a526da78cc24ac470ff8f113341
cf16 <cf16rocket@gmail.com>
2014-06-19 21:44:53 +0200

    mask input on terminal with asterisk

--- io/mask_cin_imput/main.cpp
+++ io/mask_cin_imput/main.cpp
@@ -1,33 +1,40 @@
 /* 
  * File:   main.cpp
- * Author: root
+ * Author: piter cf16 eu
  *
  * Created on June 19, 2014, 8:48 PM
+ * 
+ * Here we are masking input with asterisk.
+ * We use tcgetattr/tcsetattr to get and set
+ * terminal attributes so echo is disabled
+ * on stdin and we print "*" instead on
+ * each character user input.
  */
 
 #include <iostream>
 #include <string>
 #include <termios.h>
-
-
 #include <stdio.h>
 
+/* get/set attributes of termional*/
 int getch() {
+    
     struct termios oldtc, newtc;
     int ch;
-    tcgetattr(STDIN_FILENO, &oldtc);
+    tcgetattr( STDIN_FILENO, &oldtc);
     newtc = oldtc;
     newtc.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newtc);
+    tcsetattr( STDIN_FILENO, TCSANOW, &newtc);
     ch=getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldtc);
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldtc);
+    
     return ch;
 }
 
 
-int main(int argc, char **argv) {
+int main( int argc, char **argv) {
     int ch;
-    printf("Press ENTER to exit.\n\n");
+    printf( "Press ENTER to exit.\n\n");
     for (;;) {
         ch = getch();
 
