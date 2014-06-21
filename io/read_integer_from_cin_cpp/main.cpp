93a4828a3805c859808bab4c664f32a5b9d043c0
cf16 <cf16rocket@gmail.com>
2014-06-21 23:04:12 +0200

    Custom fast integer input ( from cin) function

---
+++ io/read_integer_from_cin_cpp/main.cpp
@@ -0,0 +1,47 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on June 21, 2014, 10:57 PM
+ * 
+ * Custom fast input function
+ * http://stackoverflow.com/questions/24345663/different-results-for-long-long-int-in-c-and-c?noredirect=1#24345758
+ */
+
+#include <stdio.h>
+#include <iostream>
+
+
+
+/*
+ * Custom fast input function
+ */
+int read_int()
+{
+    char c = getchar();
+    while(c<'0' || c>'9')
+    c = getchar();
+    int ret = 0;
+    while(c>='0' && c<='9')
+    {
+        ret = 10 * ret + c - 48;
+        c = getchar();
+    }
+    return ret;
+}
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    /* Disable sync with iostreams to get some speed */
+    std::ios::sync_with_stdio(false);
+    
+    int x;
+    std::cin >> x;
+    std::cout << x + 1003;
+    
+    return 0;
+}
+
