8a8883f5a0631a2756f3b53f7d959069d671cbc9
cf16 <cf16rocket@gmail.com>
2014-06-21 12:38:16 +0200

    cases when float x == x - 1

---
+++ numbers/float_x_subtruct_one_equal_x_cpp/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on June 21, 2014, 12:27 PM
+ * 
+ * Cases when x - 1 can't be represented other that x
+ */
+
+#include <iostream>
+
+int main() {
+    for( unsigned shift = 0; shift < 32; ++shift) {
+        float base = 1u << shift;
+        float other = base-1.0;
+        std::cout << base << ( base == other? "==" : "!=") << other << '\n';
+    }
+}
+
