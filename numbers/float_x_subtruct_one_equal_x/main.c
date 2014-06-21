83d12769e36a678c6500db26d77187d5a327084d
cf16 <cf16rocket@gmail.com>
2014-06-21 12:42:08 +0200

    cases when float x == x - 1: have a nice day

--- numbers/float_x_subtruct_one_equal_x/main.c
+++ numbers/float_x_subtruct_one_equal_x/main.c
@@ -6,6 +6,9 @@
  * 
  * Cases when x - 1 can't be represented other that x
  * Have a nice day.
+ * 
+ * http://stackoverflow.com/questions/15440052/largest-number-x
+ * http://coliru.stacked-crooked.com/view?id=cc0ae3f34df4c559275ba1290458f077-61c3814520a8d4318f681038dc4b4da7
  */
 
 #include <stdio.h>
