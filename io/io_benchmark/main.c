ad69eedecbbdf828cc4050aa29a6d4657cc1440a
mucha tse tse <cf16rocket@gmail.com>
2014-10-07 19:13:35 +0100

    comments

--- io/io_benchmark/main.c
+++ io/io_benchmark/main.c
@@ -3,6 +3,43 @@
  * Author: peterg
  *
  * Created on October 5, 2014, 1:41 AM
+ *
+ * Measure & compare io in C or C++ environment.
+ 
+ Results on x64 Windows8 IntelCore2Duo 3.16GHz
+ $ perl run_numbers.pl 
+Number of runs:3
+Number of numbers to process:500000
+C:
+real    0m8.352s
+user    0m7.035s
+sys     0m0.015s
+
+C++:
+real    0m9.096s
+user    0m7.737s
+sys     0m0.015s
+-----------------------------------------
+C:
+real    0m8.270s
+user    0m7.144s
+sys     0m0.031s
+
+C++:
+real    0m9.049s
+user    0m7.893s
+sys     0m0.015s
+-----------------------------------------
+C:
+real    0m8.168s
+user    0m7.160s
+sys     0m0.015s
+
+C++:
+real    0m9.021s
+user    0m7.862s
+sys     0m0.046s
+-----------------------------------------
  */
 
 #include <stdio.h>
