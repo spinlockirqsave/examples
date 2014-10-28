25845c24d4bcdd968974703d13acde307dee3ffc
cf16 <cf16rocket@gmail.com>
2014-10-28 20:03:07 +0100

    emulate virtual mechanism using typeinfo (typeid)

---
+++ oop_techniques/virtual_emulation_2/classes.h
@@ -0,0 +1,33 @@
+/* 
+ * File:   classes.h
+ * Author: peterg
+ *
+ * Created on October 27, 2014, 12:16 AM
+ */
+
+#ifndef CLASSES_H
+#define	CLASSES_H
+
+
+class A;
+class B;
+class C;
+
+class Object {
+public:
+};
+
+class A : public Object {
+public:
+};
+
+class B  : public Object {
+public:
+};
+
+class C  : public Object {
+public:
+};
+
+#endif	/* CLASSES_H */
+
---
+++ oop_techniques/virtual_emulation_2/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <typeinfo>
+
+#include "classes.h"
+
+class Object;
+
+
+namespace {
+
+    void ab( Object &obj1, Object &obj2);
+
+    typedef void ( *hit_f_ptr)( Object& obj1, Object& obj2);
+    typedef std::map< std::pair< std::string, std::string>, hit_f_ptr> HitMap;
+
+    HitMap *init_hit_map()
+    {
+        HitMap *phm = new HitMap;
+        ( *phm)[ std::make_pair( "A", "B")] = &ab;
+        
+        return phm;
+    }
+
+    hit_f_ptr lookup( const std::string& s1, const std::string &s2)
+    {
+        static std::auto_ptr<HitMap> aphm( init_hit_map()); 
+        HitMap::const_iterator it = aphm->find( std::make_pair( s1, s2));
+        if( it == aphm->end())
+            return 0;
+        return ( *it).second;
+    }
+
+    void collide( Object& obj1, Object &obj2)
+    {
+        hit_f_ptr phf = lookup( typeid(obj1).name(), typeid(obj2).name());
+        if( phf == 0)
+            std::cout << "oh, dear";
+        phf( obj1, obj2);
+    }
+}
+
+namespace {
+    void ab( Object &obj1, Object &obj2)
+    {
+        std::cout << "ab:" << typeid(obj1).name() << "," << typeid( obj2).name();
+    }
+}
+
+int main()
+{
+    A a;
+    B b;
+    collide( a, b);
+}
