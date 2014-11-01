88470e2170c16251c52fde5ef4241beb4d54aaca
cf16 <cf16rocket@gmail.com>
2014-11-01 12:49:01 +0100

    operator= overloading & inheritance

--- oop_techniques/operator=/main.cpp
+++ oop_techniques/operator=/main.cpp
@@ -36,6 +36,56 @@ public:
     }
 };
 
+/*--------------improvement-----------------------------*/
+class Animal2 {
+public:
+    virtual Animal2& operator= ( const Animal2& o) { std::cout << "Animal2::operator=\n"; return *this; }
+};
+
+class Lizard2 : public Animal2 {
+public:
+    virtual Lizard2& operator= ( const Animal2& o) { std::cout << "Lizard2Animal2::operator=\n"; return *this; }
+    Lizard2& operator= ( const Lizard2& o) {
+        std::cout << "Lizard2::operator=\n";
+        return *this;
+    }
+};
+
+class Chicken2 : public Animal2 {
+public:
+    virtual Chicken2& operator= ( const Animal2& o) { std::cout << "Chicken2Animal2::operator=\n"; return *this; }
+    Chicken2& operator= ( const Chicken2& o) {
+        Animal2::operator=( o);
+        std::cout << "Chicken2::operator=\n";
+        return *this;
+    }
+};
+
+/*--------------improvement2-----------------------------*/
+class Animal3 {
+protected:            // now cross platform Chicken-to-Liazrd assignment is disabled!
+    virtual Animal3& operator= ( const Animal3& o) { std::cout << "Animal3::operator=\n"; return *this; }
+};
+
+class Lizard3 : public Animal3 {
+public:
+    virtual Lizard3& operator= ( const Animal3& o) { std::cout << "Lizard3Animal3::operator=\n"; return *this; }
+    Lizard3& operator= ( const Lizard3& o) {
+        std::cout << "Lizard3::operator=\n";
+        return *this;
+    }
+};
+
+class Chicken3 : public Animal3 {
+public:
+    virtual Chicken3& operator= ( const Animal3& o) { std::cout << "Chicken3Animal3::operator=\n"; return *this; }
+    Chicken3& operator= ( const Chicken3& o) {
+        Animal3::operator=( o);
+        std::cout << "Chicken3::operator=\n";
+        return *this;
+    }
+};
+
 /*
  * 
  */
@@ -48,10 +98,25 @@ int main(int argc, char** argv) {
     Animal *apc = &c;
 
     *apl = *apc; // only Animal::operator=,                     virtual call
+                 // and we just assignerd a chicken to lizard !!!!!!!!!!!!!
     l = l;       // Lizard::operator=,                          static call       
     
     *apc = *apl; // only Animal::operator=                      virtual call
     c = c;       // Animal::operator= + Chicken::operator=      static call
+
+    /*--------------improvement-----------------------------*/
+    Lizard2 l2;
+    Chicken2 c2;
+
+    Animal2 *apl2 = &l2;
+    Animal2 *apc2 = &c2;
+
+    *apl2 = *apc2; // virtual Lizard2& operator= ( const Animal2& o),                   virtual call
+                 // and we just assignerd a chicken to lizard !!!!!!!!!!!!!
+    l2 = l2;       // Lizard2& operator= ( const Lizard2& o)                            static call       
+    
+    *apc2 = *apl2; // virtual Chicken2& operator= ( const Animal2& o)                   virtual call
+    c2 = c2;       // Chicken2& operator= ( const Chicken2& o)                          static call
 
     return 0;
 }
