40b7159cfd3eac295d15b0d31b12e06ea4827f69
cf16 <cf16rocket@gmail.com>
2014-11-22 23:13:03 +0100

    case insensitive

---
+++ oop_techniques/string_case_insensitive/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: peterg
+ *
+ * Created on November 22, 2014, 11:09 PM
+ */
+
+#include <iostream>
+
+/*
+ * 
+ */
+int main( int argc, char** argv) {
+
+    return 0;
+}
+
