fde714e9055a160a9b94d41b49a9581f4877a29c
cf16 <cf16rocket@gmail.com>
2015-02-28 18:20:10 +0100

    mapping NULL pointer

---
+++ hacker/null_mmap/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/mman.h>
+
+
+int main()
+{
+    int *p = NULL; // target
+    if ( mmap( NULL, 4096, PROT_WRITE|PROT_READ, MAP_PRIVATE|MAP_ANONYMOUS|MAP_FIXED, -1, 0) == MAP_FAILED)
+    {
+        perror( "Mapping NULL failed");
+        fprintf( stderr, "Is /proc/sys/vm/mmap_min_addr non-zero?");
+        return -1;
+    }
+
+    printf( "Dereferencing now NULL yields:%d\n", *p);
+    *p = 7;
+    printf( "After writting to NULL, now *NULL=%d\n", *p);
+
+    return 0;
+}
